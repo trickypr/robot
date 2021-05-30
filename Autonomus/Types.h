a4a93c162873200fdae584a51d3dfa40bc57e43a
trickypr <trickypr@icloud.com>
2021-05-30 12:27:45 +1000

    🔥 Remove unused code

--- Autonomus/Types.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// This file contains type aliases to improve readability. These are partially
-// based on rust types because rust types are the best types
-
-using int8 = char;
-using uint8 = unsigned int8;
